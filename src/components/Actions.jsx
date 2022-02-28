6a43a971918ab38125d8e8b26e8138b753981826
Joao Balico <joaobalico12@hotmail.com>
2022-02-28 18:40:31 +0000

    Solved

--- src/components/Actions.jsx
+++ src/components/Actions.jsx
@@ -3,12 +3,13 @@ import React from "react";
 function Actions(props) {
   return (
     <div>
-      <link
-        rel="stylesheet"
-        href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
-        integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p"
-        crossorigin="anonymous"
-      />
+      <div className="actions">
+          {/* Font Awesome icons */}
+          <i class="far fa-comment"></i>
+          <i class="fas fa-retweet"></i>
+          <i class="far fa-heart"></i>
+          <i class="fas fa-share"></i>
+        </div>
     </div>
   );
 }
