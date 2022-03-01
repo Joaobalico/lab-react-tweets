c97388f3e5492cd11107a6eda823abd76ec455a1
Joao Balico <joaobalico12@hotmail.com>
2022-03-01 14:21:28 +0000

    Finish iteration 9

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function User(props) {
   return (
     <div>
-        <span>{props.userData.name}</span>
+        <span> <b>{props.userData.name}</b> </span>
         <span>{props.userData.handle}</span>
     </div>
   )
